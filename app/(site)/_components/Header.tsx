f9c44296d958b87241fccf58011424849199b3ec
shresthapradhuman <shresthapradhuman2018@gmail.com>
2025-04-02 01:27:05 +0900

    build hero section

--- app/(site)/_components/Header.tsx
+++ app/(site)/_components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <header className="sticky z-50 w-full border-b bg-background top-0 shadow">
-      <nav className="container px-4  lg:px-12 mx-auto h-16 flex items-center justify-between">
+      <nav className="container px-4 lg:px-12 mx-auto h-16 flex items-center justify-between">
         {/* logo section */}
         <Logo />
         {/* user navigation section */}
