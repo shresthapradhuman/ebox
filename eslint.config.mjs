ee02165072402d8e215c284caed3f0b32de745b8
shresthapradhuman <shresthapradhuman2018@gmail.com>
2025-04-07 23:58:20 +0900

    Update ESLint configuration to remove unused extensions and add ignoreDuringBuilds option in Next.js config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: [
-      "next/core-web-vitals",
-      "next/typescript",
-      "standard",
-      "plugin:tailwindcss/recommended",
-      "prettier",
-    ],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
