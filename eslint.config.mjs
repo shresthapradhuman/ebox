9abf6e9784da655226e98f53e8cc2794094510eb
shresthapradhuman <shresthapradhuman2018@gmail.com>
2025-04-07 23:21:21 +0900

    Intial Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "standard",
+      "plugin:tailwindcss/recommended",
+      "prettier",
+    ],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
